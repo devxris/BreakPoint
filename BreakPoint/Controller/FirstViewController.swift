de1218e45a391399175e7061cb29823a49c51fd8
devxris <chuwei0915@gmail.com>
2017-10-30 22:41:27 +0800

    MVC setup

---
+++ BreakPoint/Controller/FirstViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 30/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+
+}
+
---
+++ BreakPoint/Controller/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 30/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+
+}
+
