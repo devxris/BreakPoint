19a4fb43b83bbdf7762d99fdfd5a712d3194c1eb
devxris <chuwei0915@gmail.com>
2017-10-31 17:28:25 +0800

    Add MeVC and setup UI

---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,22 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 31/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+	// outlets
+	@IBOutlet weak var profileImage: UIImageView!
+	@IBOutlet weak var email: UILabel!
+	@IBOutlet weak var tableView: UITableView!
+	
+	// target actions
+	@IBAction func signout(_ sender: UIButton) {
+		
+	}
+}
