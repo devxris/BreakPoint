a0240ccee6cc91ca67e34ff2f212af2fee3e5bdd
devxris <chuwei0915@gmail.com>
2017-10-31 16:10:15 +0800

    add AuthService

---
+++ BreakPoint/Controller/LoginVC.swift
@@ -0,0 +1,14 @@
+//
+//  LoginVC.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 31/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+
+}
---
+++ BreakPoint/Services/AuthService.swift
@@ -0,0 +1,33 @@
+//
+//  AuthService.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 31/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+	
+	static let instance = AuthService()
+	typealias CompletionHandler = (_ status: Bool, _ error: Error?) -> Void
+	
+	func registerUser(with email: String, and password: String, completion: @escaping CompletionHandler) {
+		Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+			
+			guard let user = user else { completion(false, error); return }
+			let userData: [String: Any] = ["provider": user.providerID,
+			                               "email": user.email as Any]
+			DataService.instance.createDBUser(uid: user.uid, userData: userData)
+		}
+	}
+	
+	func loginUser(with email: String, and password: String, completion: @escaping CompletionHandler) {
+		Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+			guard let _ = user else { completion(false, error); return }
+			completion(true, nil)
+		}
+	}
+}
