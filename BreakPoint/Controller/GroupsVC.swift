fecd28ea16002b642e0cb7e550b94fe11ef55f1c
devxris <chuwei0915@gmail.com>
2017-11-05 16:04:02 +0800

    build group cell

--- BreakPoint/Controller/GroupsVC.swift
+++ BreakPoint/Controller/GroupsVC.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class GroupsVC: UIViewController {
 
-
+	@IBOutlet weak var groupTable: UITableView! {
+		didSet {
+			groupTable.dataSource = self
+			groupTable.delegate = self
+		}
+	}
 }
 
+extension GroupsVC: UITableViewDataSource, UITableViewDelegate {
+	
+	func numberOfSections(in tableView: UITableView) -> Int { return 1 }
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 4
+	}
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! GroupCell
+		cell.configure(title: "Groups title", description: "Just fake one", membersCount: 5)
+		return cell
+	}
+}
---
+++ BreakPoint/View/GroupCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupCell.swift
+//  BreakPoint
+//
+//  Created by Chris Huang on 05/11/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+	// outlets
+	@IBOutlet weak var groupTitle: UILabel!
+	@IBOutlet weak var groupDescription: UILabel!
+	@IBOutlet weak var members: UILabel!
+	
+	func configure(title: String, description: String, membersCount: Int) {
+		self.groupTitle.text = title
+		self.groupDescription.text = description
+		self.members.text = String(membersCount)
+	}
+}
